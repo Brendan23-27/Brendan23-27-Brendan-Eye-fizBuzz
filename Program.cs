63ddf9120c29084b0172e425db8950a63768a819
Brendan23-27 <156932485+Brendan23-27@users.noreply.github.com>
2024-10-15 16:05:00 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace FizBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+             GetNumbers();
+        }
+
+        static void GetNumbers()
+        {
+
+
+            int i;
+
+            for ( i = 1; i <= 100; i++)
+            {
+                UseNumbers(i);
+
+               
+            }
+;  
+        }
+
+        static int UseNumbers(int i)
+        {
+           
+
+            if (i % 3 == 0)
+            {
+               if (i % 5 == 0)
+                {
+                 Console.WriteLine("FizBuzz");
+
+                }
+                Console.WriteLine();
+            }
+
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            if (i % 3 == 0)
+            {
+                Console.WriteLine("Fiz");
+            }
+
+            else
+            {
+                Console.WriteLine(i);
+            }
+            return i;
+        }
+
+    }
+}
+
+
